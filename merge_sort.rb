48dacde096bc7b5ded171d227a92ca1ef06e6710
Akshat <akshatjaiswal729@gmail.com>
2021-09-23 14:06:39 +0530

    second commit

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,7 +2,13 @@ def merge_sort (arr)
     return arr if arr.length == 1
     left = merge_sort(arr[0...(arr.length / 2 )])
     right = merge_sort(arr[(arr.length / 2)..(arr.length - 1)])
-    return left + right
+
+    sort = []
+    until left.empty? || right.empty? 
+        left[0] <= right[0] ? sort << left.shift : sort << right.shift 
+    end
+
+    return sort + left + right
 end
 
 array = [5, 2, 1, 3, 6, 4]
